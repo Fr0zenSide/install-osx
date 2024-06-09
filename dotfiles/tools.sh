7f3e8b87a9f8af21cecb5fe42d83ce783dbb109f
Jeoffrey Thirot <jeoffrey.thirot@gmail.com>
2024-06-09 19:07:31 +0200

    update config with dotfiles folder

--- dotfiles/tools.sh
+++ dotfiles/tools.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+ff () {
+    TF_PYTHONIOENCODING=$PYTHONIOENCODING;
+    export TF_SHELL=zsh;
+    export TF_ALIAS=ff;
+    TF_SHELL_ALIASES=$(alias);
+    export TF_SHELL_ALIASES;
+    TF_HISTORY="$(fc -ln -10)";
+    export TF_HISTORY;
+    export PYTHONIOENCODING=utf-8;
+    TF_CMD=$(
+        thefuck THEFUCK_ARGUMENT_PLACEHOLDER $@
+    ) && eval $TF_CMD;
+    unset TF_HISTORY;
+    export PYTHONIOENCODING=$TF_PYTHONIOENCODING;
+    test -n "$TF_CMD" && print -s $TF_CMD
+}
 
 if ! command -v fzf &> /dev/null
 then
