a5ca6809a3b2e667fafe1fcc7afe72525d9bb93c
Jeoffrey Thirot <jeoffrey.thirot@gmail.com>
2026-02-04 11:04:28 +0100

    update emacs settings to use the right theme and init config file. !! remove ~/.emacs or file in the user project if not gonna search ~/.config/emacs files!

---
+++ dotfiles/.config/emacs/init.el
@@ -0,0 +1,30 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(catppuccin-theme lsp-mode swift-mode websocket ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'catppuccin :no-confirm)
+(setq catppuccin-flavor 'frappe) ;; or 'latte, 'macchiato, or 'mocha
+(catppuccin-reload)
+
+;; If I want to switch with dracula theme
+;; Install using MELPA
+;; M-x package-install <RET> dracula-theme
+;; Or install with local files
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;; Activate it
+;; (load-theme 'dracula t)
+
+(package-initialize)
